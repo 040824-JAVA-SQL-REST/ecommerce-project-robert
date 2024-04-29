be5d1773cd09cd9cca28cb426b1e0f2357b013bb
robj1925 <robj1925@gmail.com>
2024-04-29 09:58:35 -0400

    working on tests

--- p0/src/test/java/com/revature/p0/ecommerce/services/UserServiceTest.java
+++ p0/src/test/java/com/revature/p0/ecommerce/services/UserServiceTest.java
@@ -1,5 +1,11 @@
 package com.revature.p0.ecommerce.services;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -10,11 +16,9 @@ import com.revature.ecommercep0.model.User;
 import com.revature.ecommercep0.service.RoleService;
 import com.revature.ecommercep0.service.UserService;
 
-
 public class UserServiceTest {
     private RoleService roleService;
     private UserService userService;
-    private UserServiceTest userServiceTest;
     private UserDao userDao;
 
     @Before
@@ -23,21 +27,74 @@ public class UserServiceTest {
         userDao = Mockito.mock(UserDao.class);
         userService = new UserService(userDao, roleService);
     }
-    
     @Test
     public void isValidEmailShouldReturnTrueIfEmailisValid() {
+        // AAA , Arrange, Act, Assert
+
+        // Arrange
         String validEmail = "robert@yahoo.net";
+        boolean expected = true;
 
-        boolean actual  = userService.isValidEmail(validEmail);
+        // Actual
+        boolean actual = userService.isValidEmail(validEmail);
 
-       // assertTrue(actual);
+        // Assert
+        // assertEquals(expected, actual);
+
+        // In this case we dont need expected because we know it needs to be true so
+        // just use AssertTRue()
+        assertTrue(actual);
     }
+
     @Test
     public void isValidEmailShouldReturnFalseIfEmailisBlank() {
-        String validEmail = "robert@yahoo.net";
+        String validEmail = "";
 
-        boolean actual  = userService.isValidEmail(validEmail);
+        boolean actual = userService.isValidEmail(validEmail);
 
-        // assertFalse(actual);
+         assertFalse(actual);
+    }
+    @Test
+    public void isUniqueEmailShouldReturnTrueIfEmailIsUnique() {
+
+        String uniqueUsername = "robert@yahoo.net";
+
+        List<User> mockedUsers = List.of(new User("lol@yahoo.net", "1", "2", "3"), new User("23", "23", "#4", "34"));
+
+        Mockito.when(userDao.findAll()).thenReturn(mockedUsers);
+
+        boolean actual = userService.isUniqueEmail(uniqueUsername);
+
+        assertTrue(actual);
+    }
+    @Test
+    public void isUniqueEmailShouldReturnFalseIfEmailIsNotUnique() {
+        String notUniqueEmail = "robert@yahoo.net";
+
+
+        List<User> mockedUsers = List.of(new User("robert@yahoo.net", "1", "2", "3"), new User("23", "23", "#4", "34"));
+        Mockito.when(userDao.findAll()).thenReturn(mockedUsers);
+
+        boolean actual = userService.isUniqueEmail(notUniqueEmail);
+
+        assertFalse(actual);
+
+    }
+    @Test
+    public void isValidPasswordShouldReturnTrueIfPasswordIsValid() {
+        String validPassword = "password123";
+
+        boolean actual = userService.isValidPassword(validPassword);
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void isValidPasswordShouldReturnFalseIfPasswordIsNotValid() {
+        String notValidPassword = "";
+
+        boolean actual = userService.isValidPassword(notValidPassword);
+
+        assertFalse(actual);
     }
 }
